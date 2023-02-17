61533b7ea1bf0e1e67dfbf9a7ac951d0dc2517f4
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-02-17 08:36:15 +0000

    create stack pop and push

--- .ccls-cache/@home@runner@stack-u6513115/stack.h
+++ .ccls-cache/@home@runner@stack-u6513115/stack.h
@@ -3,12 +3,35 @@
 #define stack_h
 #include "node.h"
 typedef struct {
-	NodePtr top;
-	int size;
-}Stack;
+  NodePtr top;
+  int size;
+} Stack;
 
-typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(StackPtr s);
-void pop_all(StackPtr s);
+typedef Stack *StackPtr;
+void push(StackPtr s, int x) {
+  Node *new_node = (NodePtr)malloc(sizeof(Node));
+  if (new_node) {
+    new_node->data = x;
+    new_node->nextPtr = s->top;
+    s->top = new_node;
+    s->size++;
+  }
+}
+
+int pop(StackPtr s) {
+  NodePtr t = s->top;
+  if (t != NULL) {       // if(t)
+    int value = t->data; //(*top)->data
+    s->top = t->nextPtr; //(*top)->nextPtr
+    s->size--;
+    free(t);
+    return value;
+  }
+  printf("Empty Stack\n");
+  return 0;
+}
+void pop_all(StackPtr s)//เอา pop ออก จน size เป็น 0
+{
+  
+}
 #endif
