996c352fd92123989bbb1235e167d98874b67046
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-02-17 08:02:05 +0000

    create loop push and pop

--- .ccls-cache/@home@runner@stack-u6513115/main.c
+++ .ccls-cache/@home@runner@stack-u6513115/main.c
@@ -1,13 +1,26 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include "node.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv){
-  
-  int i,N,j;
-  NodePtr top=NULL;
+int main(int argc, char **argv) {
 
+  int i, N, j;
+  NodePtr top = NULL;
+
+  printf("****************************\n");
+  printf("atoi(argv[i]) = ");
+  for (i = 1; i < argc; i++) // argc 0 is program name
+  {
+    push(&top, atoi(argv[i]));
+    printf("%d\t", atoi(argv[i]));
+  }
+  printf("****************************\n");
+  while (top) {
+    printf("%d\n", pop(&top));
+  }
+
+  /*
   printf("****************************\n");
   push(&top,5);
   printf("%d\n",pop(&top));
@@ -15,27 +28,22 @@ int main(int argc, char **argv){
   push(&top,8);
   printf("%d\n",pop(&top));
   printf("%d\n",pop(&top));
-  printf("%d\n",pop(&top));
- 
- 
-  
+  printf("%d\n",pop(&top));*/
 
- /*
- Stack s;
- printf("Checking the parentheses in argv arguments\n");
-  for(i=1;i<argc;i++){
-   
-     for(j=0;j<strlen(argv[i]);j++){
-        Use stack to help with the parentheses
+  /*
+  Stack s;
+  printf("Checking the parentheses in argv arguments\n");
+   for(i=1;i<argc;i++){
+
+      for(j=0;j<strlen(argv[i]);j++){
+         Use stack to help with the parentheses
 
 
-     }
+      }
 
 
-  }
-*/
+   }
+ */
 
-
-
-   return 0;
+  return 0;
 }
