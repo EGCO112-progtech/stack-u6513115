1562aa03ad6e7d4857afe3abc9c4f276c0e5d332
u6513115 <122536267+u6513115@users.noreply.github.com>
2023-02-17 07:11:05 +0000

    default

--- .ccls-cache/@home@runner@stack-u6513115/main.c
+++ .ccls-cache/@home@runner@stack-u6513115/main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv){
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
-       /* Use stack to help with the parentheses
+       /* Use stack to help with the parentheses*/
 
 
      }
